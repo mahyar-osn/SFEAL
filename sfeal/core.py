9e8edbfb9a9907df8414b7ac07a356be9f05806a
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2018-03-16 20:27:29 +1300

    Added a method called export_to_cm where a user can convert their mesh to cmiss format and save it.

--- sfeal/core.py
+++ sfeal/core.py
@@ -1,9 +1,11 @@
 import numpy
 import morphic
 
+
 class SSM(object):
 
     def __init__(self):
+
         self.X = []
         self.input_mesh = None
         self.groups = None
@@ -17,13 +19,14 @@ class SSM(object):
         self.mean = None
         self.score_z = None
         self.SD = None
-
+        self.nodes = None
         self.new_data = []
         self.weights = []
+        self.lung = None
 
-    def add_mesh(self, mesh, index = 0):
+    def add_mesh(self, mesh, index=0):
         mesh = morphic.Mesh(str(mesh))
-        if self.input_mesh == None:
+        if self.input_mesh is None:
             self.input_mesh = mesh
         if isinstance(mesh, str):
             mesh = morphic.Mesh(mesh)
@@ -40,7 +43,7 @@ class SSM(object):
 
         self.X.append(x)
 
-    def pca_setup(self, num_modes = 2):
+    def pca_setup(self, num_modes=2):
         from sklearn import decomposition
         self.X = numpy.array(self.X)
         self.num_modes = num_modes
@@ -80,7 +83,8 @@ class SSM(object):
                     if isinstance(node, morphic.mesher.StdNode):
                         self.mesh.add_stdnode(node.id, node.values)
                     elif isinstance(node, morphic.mesher.DepNode):
-                        self.mesh.add_depnode(node.id, node.element, node.node, shape=node.shape, scale=node.scale)
+                        self.mesh.add_depnode(node.id, node.element, node.node,
+                                              shape=node.shape, scale=node.scale)
                     if isinstance(node, morphic.mesher.PCANode):
                         raise Exception('Not implemented')
 
@@ -138,7 +142,7 @@ class SSM(object):
         pca_components = pca.components_.T
         pca_variance = pca.explained_variance_
         pca_explained_variance = pca.explained_variance_ratio_
-        self.ratio = {'MODE_{}'.format(m + 1):'{:.2f}'.format(float(pca_explained_variance[m]))
+        self.ratio = {'MODE_{}'.format(m + 1): '{:.2f}'.format(float(pca_explained_variance[m]))
                       for m in range(len(pca_explained_variance))}
         dataset = dict()
         for i in range(len(subjectStore)):
@@ -251,23 +255,194 @@ class SSM(object):
 
         return self.z_score
 
-    def export_to_cm(self, pmesh, weights):
+    def export_to_cm(self, pmesh, weights, name='default', lung='l', show_mesh=False):
         if not self.weights:
             pass
         else:
             self.weights = []
 
+        if lung == 'R':
+            self.lung = 'Right'
+        elif lung == 'L':
+            self.lung = 'Left'
+        else:
+            raise Exception("'lung' argument can ONLY be L OR R!")
+
         self.weights = weights
         self.pmesh = pmesh
-        self.pmesh.nodes['weights'].values[1:] = 0
-        self.pmesh.nodes['weights'].values[0] = 1 # average
+        self.pmesh.nodes['weights'].values[1:] = 0  # reset weights to zero
+        self.pmesh.nodes['weights'].values[0] = 1  # adding average
         for numMode in range(len(self.weights)):
-            self.pmesh.nodes['weights'].values[numMode+1] = self.weights[numMode]
+            self.pmesh.nodes['weights'].values[numMode + 1] = self.weights[numMode]
             self.pmesh.update_pca_nodes()
 
+        # saving
+        import os
+        import pandas as pd
+        import subprocess
+        from useful_files import nodes
 
+        if self.nodes is not None:
+            print "SHOULDN'T BE HERE!"
+            self.nodes = None
 
+        self.nodes = nodes.Nodes()
 
+        output_dir = 'output/export_to_cm/%s' % name
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
+        input_folder = '../useful_files'
 
+        path_to_export_mesh = output_dir
+        temp_file = '%s_reconstructed_temp.csv' % self.lung
+        output_file = '%s_reconstructed' % self.lung
+        save_temp_file = output_dir + '/%s' % temp_file
+        save_output_file = output_dir + '/%s' % output_file
+        ipnode_file = '%s_reconstructed' % self.lung
+        path_to_ipnode_file = '%s/%s' % (output_dir, ipnode_file)
 
+        path_to_com_file = os.path.join(os.path.dirname(__file__),
+                                        input_folder, 'perl_com', 'ipnode2exnode.com')
+        path_to_cmgui_file = os.path.join(os.path.dirname(__file__),
+                                        input_folder, 'perl_com', 'cmgui.com')
+        ip2ex_perl = os.path.join(os.path.dirname(__file__),
+                                  input_folder, 'perl_com', 'ipnode2exnode.pl')
+        ip2ex_cm = os.path.join(os.path.dirname(__file__),
+                                input_folder, 'perl_com', 'ipnode2exnode')
+        cmgui_file = os.path.join(os.path.dirname(__file__),
+                                  input_folder, 'perl_com', 'cmgui')
+        param_file = os.path.join(os.path.dirname(__file__),
+                                  input_folder, 'perl_com', '3d_fitting')
+        versions_file = os.path.join(os.path.dirname(__file__),
+                                input_folder, 'perl_com', 'versions')
+        base_file = os.path.join(os.path.dirname(__file__),
+                                  input_folder, 'perl_com', 'BiCubic_Surface_Unit')
+
+        if self.lung == 'Right':
+            node_file = 'nodes_%s.csv' % self.lung
+            input_file = os.path.join(os.path.dirname(__file__),
+                                      input_folder, node_file)
+            elem_file = os.path.join(os.path.dirname(__file__),
+                                     input_folder, 'perl_com', 'templateRight')
+
+            nodes = self.nodes.set_nodes(lung='right')
+            for node_number in nodes:
+                node = self.pmesh.nodes[node_number]
+                nodeValues = node.values
+                with open(save_temp_file, 'a') as f:
+                    numpy.savetxt(f, nodeValues)
+
+            a = pd.read_csv(input_file)
+            b = pd.read_csv(save_temp_file, delimiter=' ')
+            result = pd.concat([a, b], axis=1)
+            result.to_csv('%s.csv' % save_output_file, sep=' ', index=False)
+            os.remove(save_temp_file)
+            py2ip_right_file = os.path.join(os.path.dirname(__file__),
+                                            input_folder, 'perl_com', 'py2ip_right.pl')
+
+            subprocess.call(["perl", py2ip_right_file, "%s.csv" % save_output_file,
+                             "%s.ipnode" % path_to_ipnode_file])
+
+            with open(path_to_com_file, 'wb') as comfile:
+                comfile.write(" set echo on;\n")
+                comfile.write(" fem def param;r;{0}".format("%s;\n" % param_file))
+                comfile.write(" fem def coor;r;{0}".format("%s;\n" % versions_file))
+                comfile.write(" fem def base;r;{0}".format("%s;\n" % base_file))
+                comfile.write(" fem def node;r;{0}".format("%s;\n" % path_to_ipnode_file))
+                comfile.write(" fem def elem;r;{0}".format("%s;\n" % elem_file))
+                comfile.write(" fem export node;{0} as {1};\n".format("%s" % path_to_ipnode_file, self.lung))
+                comfile.write(" fem export elem;{0} as {1};\n".format("%s" % path_to_ipnode_file, self.lung))
+                comfile.write(" fem def node;w;{0}".format("%s;\n" % path_to_ipnode_file))
+                comfile.write(" fem quit;\n")
+
+            if show_mesh:
+                with open(path_to_cmgui_file, 'wb') as comfile:
+                    comfile.write(" gfx read node {0}".format("'%s';\n" % path_to_ipnode_file))
+                    comfile.write(" gfx read elem {0}".format("'%s';\n" % path_to_ipnode_file))
+                    comfile.write(" gfx cre egroup fissure;\n")
+                    comfile.write(" gfx mod egroup fissure add 51..62;\n")
+                    comfile.write(" gfx mod g_e {0} general clear circle_discretization 6 default_coordinate coordinates; element_discretization '12*12*12' native_discretization none;\n".format("'%s'" % self.lung))
+                    comfile.write(" gfx mod g_e {0} lines coordinate coordinates select_on material green selected_material default_selected;\n".format("'%s'" % self.lung))
+                    comfile.write(" gfx mod g_e fissure general clear circle_discretization 6 default_coordinate coordinates; element_discretization '12*12*12' native_discretization none;\n")
+                    comfile.write(" gfx mod g_e fissure surfaces material tissue;\n")
+                    comfile.write(" gfx edit scene;\n")
+                    comfile.write(" gfx cre win;\n")
+                show_cmgui = 'show'
+                subprocess.call(["perl", ip2ex_perl, "%s" % ip2ex_cm, "%s" % cmgui_file, "%s" % show_cmgui], shell=False)
+
+            else:
+                show_cmgui = 'no'
+                subprocess.call(["perl", ip2ex_perl, "%s" % ip2ex_cm, "%s" % cmgui_file, "%s" % show_cmgui], shell=True)
+
+            print "\n\t=========================================\n"
+            print "\t   ALL MESH FILES EXPORTED TO:"
+            print "\n\t\t   %s " % path_to_export_mesh
+            print "\n\t=========================================\n"
+
+            os.remove(save_output_file + '.csv')
+
+        elif self.lung == 'Left':
+            node_file = 'nodes_%s.csv' % self.lung
+            input_file = os.path.join(os.path.dirname(__file__),
+                                      input_folder, node_file)
+            elem_file = os.path.join(os.path.dirname(__file__),
+                                     input_folder, 'perl_com', 'templateLeft')
+
+            nodes = self.nodes.set_nodes(lung='left')
+            for node_number in nodes:
+                node = self.pmesh.nodes[node_number]
+                nodeValues = node.values
+                with open(save_temp_file, 'a') as f:
+                    numpy.savetxt(f, nodeValues)
+
+            a = pd.read_csv(input_file)
+            b = pd.read_csv(save_temp_file, delimiter=' ')
+            result = pd.concat([a, b], axis=1)
+            result.to_csv('%s.csv' % save_output_file, sep=' ', index=False)
+            os.remove(save_temp_file)
+
+            py2ip_left_file = os.path.join(os.path.dirname(__file__),
+                                            input_folder, 'perl_com', 'py2ip_left.pl')
+            subprocess.call(["perl", py2ip_left_file, "%s.csv" % save_output_file,
+                             "%s.ipnode" % path_to_ipnode_file])
+
+            with open(path_to_com_file, 'wb') as comfile:
+                comfile.write(" set echo on;\n")
+                comfile.write(" fem def param;r;{0}".format("%s;\n" % param_file))
+                comfile.write(" fem def coor;r;{0}".format("%s;\n" % versions_file))
+                comfile.write(" fem def base;r;{0}".format("%s;\n" % base_file))
+                comfile.write(" fem def node;r;{0}".format("%s;\n" % path_to_ipnode_file))
+                comfile.write(" fem def elem;r;{0}".format("%s;\n" % elem_file))
+                comfile.write(" fem export node;{0} as {1};\n".format("%s" % path_to_ipnode_file, self.lung))
+                comfile.write(" fem export elem;{0} as {1};\n".format("%s" % path_to_ipnode_file, self.lung))
+                comfile.write(" fem def node;w;{0}".format("%s;\n" % path_to_ipnode_file))
+                comfile.write(" fem quit;\n")
+
+            if show_mesh:
+                with open(path_to_cmgui_file, 'wb') as comfile:
+                    comfile.write(" gfx read node {0}".format("'%s';\n" % path_to_ipnode_file))
+                    comfile.write(" gfx read elem {0}".format("'%s';\n" % path_to_ipnode_file))
+                    comfile.write(" gfx cre egroup fissure;\n")
+                    comfile.write(" gfx mod egroup fissure add 111..118;\n")
+                    comfile.write(" gfx mod g_e {0} general clear circle_discretization 6 default_coordinate coordinates; element_discretization '12*12*12' native_discretization none;\n".format("'%s'" % self.lung))
+                    comfile.write(" gfx mod g_e {0} lines coordinate coordinates select_on material green selected_material default_selected;\n".format("'%s'" % self.lung))
+                    comfile.write(" gfx mod g_e fissure general clear circle_discretization 6 default_coordinate coordinates; element_discretization '12*12*12' native_discretization none;\n")
+                    comfile.write(" gfx mod g_e fissure surfaces material tissue;\n")
+                    comfile.write(" gfx edit scene;\n")
+                    comfile.write(" gfx cre win;\n")
+                show_cmgui = 'show'
+                subprocess.call(["perl", ip2ex_perl, "%s" % ip2ex_cm, "%s" % cmgui_file, "%s" % show_cmgui], shell=False)
+
+            else:
+                show_cmgui = 'no'
+                subprocess.call(["perl", ip2ex_perl, "%s" % ip2ex_cm, "%s" % cmgui_file, "%s" % show_cmgui], shell=True)
+
+            print "\n\t=========================================\n"
+            print "\t   ALL MESH FILES EXPORTED TO:"
+            print "\n\t\t   %s " % path_to_export_mesh
+            print "\n\t=========================================\n"
+
+            os.remove(save_output_file + '.csv')
+
+        return None
