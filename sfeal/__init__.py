c812d576c416bfb32dc35af5786e0d0fa1f41cd3
Mahyar Osanlouy <m.osanlouy@auckland.ac.nz>
2018-03-16 20:26:11 +1300

    Small modifications.

--- sfeal/__init__.py
+++ sfeal/__init__.py
@@ -1,10 +1,10 @@
 from core import SSM
-from visualise import FIGURE
+#from visualise import FIGURE
 
 reload_modules = True
 
 if reload_modules:
     import core
     reload(core)
-    import visualise
-    reload(visualise)
+    #import visualise
+    #reload(visualise)
